1061423971bd283f218be5029618424cd9470160
emaco <emaco@DESKTOP-ROHUIMR.home>
2020-03-11 23:43:40 +0100

    interfaccia grafica

--- src/main/java/it/polito/tdp/javafxcrashcourse/FXMLController.java
+++ src/main/java/it/polito/tdp/javafxcrashcourse/FXMLController.java
@@ -2,24 +2,37 @@ package it.polito.tdp.javafxcrashcourse;
 
 import java.net.URL;
 import java.util.ResourceBundle;
-import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 
-public class FXMLController implements Initializable {
-    
+public class FXMLController {
+
     @FXML
-    private Label label;
-    
+    private ResourceBundle resources;
+
     @FXML
-    private void handleButtonAction(ActionEvent event) {
-        System.out.println("You clicked me!");
-        label.setText("Hello World!");
+    private URL location;
+
+    @FXML
+    private TextField txtnome;
+
+    @FXML
+    private TextField txteta;
+
+    @FXML
+    private Button buttonok;
+
+    @FXML
+    private Label lbrisposta;
+
+    @FXML
+    void initialize() {
+        assert txtnome != null : "fx:id=\"txtnome\" was not injected: check your FXML file 'scene.fxml'.";
+        assert txteta != null : "fx:id=\"txteta\" was not injected: check your FXML file 'scene.fxml'.";
+        assert buttonok != null : "fx:id=\"buttonok\" was not injected: check your FXML file 'scene.fxml'.";
+        assert lbrisposta != null : "fx:id=\"lbrisposta\" was not injected: check your FXML file 'scene.fxml'.";
+
     }
-    
-    @Override
-    public void initialize(URL url, ResourceBundle rb) {
-        // TODO
-    }    
 }
